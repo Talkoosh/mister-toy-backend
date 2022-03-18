4eecc429632326efc9903e7f82c54d6fd0ae1918
Tal Koosh <talkoosh@Tals-MacBook-Air.local>
2022-03-18 16:19:10 +0200

    imporved login functionality

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -83,6 +83,7 @@ async function add(user) {
             username: user.username,
             password: user.password,
             fullname: user.fullname,
+            isAdmin: false,
             score: user.score || 0
         }
         const collection = await dbService.getCollection('user')
