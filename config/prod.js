4dc1e483d997236cd0fbe544f3109506c292c3f7
Tal Koosh <artishok.tal@gmail.com>
2022-03-21 21:48:07 +0200

    fixed backend

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+    'dbURL': 'mongodb+srv://talkoosh:<209323492Tal>@cluster0.p8c2t.mongodb.net/toy_db?retryWrites=true&w=majority',
   }
   
